f40536d945004c5cbfbc45d0b638a1887330cffa
sivalingam77 <lingams200@gmail.com>
2018-08-25 22:19:28 -0500

    Initial commit

---
+++ android/src/edu/niu/z1829451/flappybird/AndroidLauncher.java
@@ -0,0 +1,20 @@
+package edu.niu.z1829451.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import edu.niu.z1829451.flappybird.FlappyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBird(), config);
+	}
+}
